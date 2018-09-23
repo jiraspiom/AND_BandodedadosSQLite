d04fe72bc40565a3983060e2cf80df71dbc14a6e
gilmar <jiraspiom@yahoo.com.br>
2018-09-23 19:09:40 -0300

    criando bando, inserindo dado, e lendo dados.

--- app/src/main/java/br/com/example/alalgi/bandodedadossqlite/MainActivity.java
+++ app/src/main/java/br/com/example/alalgi/bandodedadossqlite/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.example.alalgi.bandodedadossqlite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        try{
+            //Criando o bando de dados com nome APP privato para que somente nosso app o veja.
+            SQLiteDatabase bancoDados = openOrCreateDatabase("App", MODE_PRIVATE , null);
+
+            //Criando tabela
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome  VARCHAR, idade int(3))");
+
+            //Inserindo dados
+            bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Gilmar', 38) ");
+            bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Borges', 109) ");
+
+            //Recuperando pessoas
+            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas", null);
+
+            
+            while(cursor != null){
+                Log.i("RESULTADO nome",cursor.getString(0));
+             Log.i("RESULTADO idade", cursor.getString(1));
+
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
